e95373a6bea620108f771981a4822c3da72574e9
BBOYADAO <bboyadao@gmail.com>
2022-11-30 22:24:37 +0700

    change entrypoint.sh for psql

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,17 @@
-#!/bin/bash
-python manage.py migrate
-python manage.py collectstatic --noinput
-echo "startting appication"
-daphne -b 0.0.0.0 -p 8000 vin_bigdata.asgi:application
+#!/bin/sh
+
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+# python manage.py flush --no-input
+# python manage.py migrate
+
+exec "$@"
